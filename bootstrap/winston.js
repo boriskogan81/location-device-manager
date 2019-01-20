0a9f862bac074040e1caf02fb1eff939c5301aea
Baruch Kogan <baruch.kogan@silverbolt.ai>
2019-01-20 11:37:54 +0200

    Added Google Cloud logging for Winston

--- bootstrap/winston.js
+++ bootstrap/winston.js
@@ -1,26 +1,23 @@
 const winston = require('winston');
+const {LoggingWinston} = require('@google-cloud/logging-winston');
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
     transports: [
-        //
-        // - Write to all logs with level `info` and below to `combined.log`
-        // - Write all logs error (and below) to `error.log`.
-        //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
-    ]
+        new winston.transports.Console(),
+        // Add Stackdriver Logging
+        loggingWinston,
+    ],
 });
 
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     logger.add(new winston.transports.Console({
+//         format: winston.format.simple()
+//     }));
+// }
 
 module.exports = logger;
